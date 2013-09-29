77f92154962aaff347502ca0f6b1c1a30f87d958
Ahmer Arif <ahmer.arif@gmail.com>
2013-09-29 10:48:42 -0400

    Allowed Hosts added to settings

--- pakcinemas/settings/base.py
+++ pakcinemas/settings/base.py
@@ -14,7 +14,7 @@ root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = ['http://dry-beyond-1890.herokuapp.com/']
+ALLOWED_HOSTS = ['dry-beyond-1890.herokuapp.com/']
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
